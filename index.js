9806950905f14c0a5104d381bf9480ecfa47da88
Edsey omenda <omendajames0317@gmail.com>
2022-06-07 15:25:05 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper';
+const welcomeMessage = 'Welcome to Flatbook, ' + currentUser+ '!' ;
+//const welcomeMessage = currentUser;
+//console.log(welcomeMessage);
+
+//const excitedWelcomeMessage = "WELCOME TO FLATBOOK, "
+//const excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + currentUser+ '!'
+const excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + currentUser.toUpperCase()+ '!'
+
+//const shortGreeting = "Welcome, "+ ':'
+//const shortGreeting = "Welcome, "+ ':'+'G'
+const shortGreeting = "Welcome, "+ ':+'+'G'+'!'
